c3ad5f7e8216ee958d747e5e770fb40051c7139c
Vanessa Manganyi <mngvan003@myuct.ac.za>
2017-04-09 17:52:03 +0200

    finished

--- Assignment1/src/SearchItLinear.java
+++ Assignment1/src/SearchItLinear.java
@@ -1,43 +1,43 @@
 /**
- *@author Vanessa Manganyi
+*@author Vanessa Manganyi
 */
 
-import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-public class SearchIt{
+public class SearchItLinear{
    public static void main(String [] args){
-    /**
-     * Uses try-catch to handle FileNotFoundExceptions.
-     * First tries to process the data but returns file not found if it can't find the file
-     */
-   try{
-      File f = new File("/home/m/mngvan003/EDT/src/testdata");
-      Scanner s = new Scanner(f);
-      BinaryTree bst = new BinaryTree();
-      
       /**
-       * loops through the original textfile and enters each line into binary tree.
-       * Changes the form of each entry to {full name; data}
+       * Uses try-catch to handle FileNotFoundExceptions. 
+       *First tries to process the data but returns file not found if it can't find the file
+      */
+      try{
+         File f = new File("/home/m/mngvan003/CSC2001F/Assignment1/src/testdata");
+         Scanner s = new Scanner(f);
+         @SuppressWarnings("Convert2Diamond")
+         ArrayList<String> sa = new ArrayList<String>(); //creates array to store information
+         
+      /**
+       * uses for-loop to enter each entry of the form {full name, data} into array
        */
-      while(s.hasNextLine()){
+         for (int i = 0; i<10000; i++){
             String line = s.nextLine();
             String[] info = line.split("\\|");
-            String proper = info[2]+","+info[1]+info[0];//System.out.print(l);
-            bst.insert(proper);
+            String proper = info[2]+","+info[1]+info[0];
+            sa.add(proper);
          }
-      
-      /**
-       * Uses while loop to compare each line in the query file and searches for the 
-       * String item in the binary search tree using the find(item) method.
-       */  
-      boolean check;
-      check = false;     
-      File fF = new File("/home/m/mngvan003/EDT/src/query");
-      Scanner inputF = new Scanner(fF);
-      
-while(inputF.hasNextLine()){
+         
+         File fF = new File("/home/m/mngvan003/EDT/src/Untitled Document 1");
+         Scanner inputF = new Scanner(fF);
+         
+         boolean check;
+         check = false;
+         /**
+          * loops through query textfile and compares each line to the items in the array
+          */
+         while(inputF.hasNextLine()){
             String l = inputF.nextLine();
             check = false;
             for(String inarray: sa){
@@ -53,11 +53,12 @@ while(inputF.hasNextLine()){
                 System.out.println("Not found");
          }
          }
-      
+         
       }
-   catch(FileNotFoundException e){
-      System.out.println("File not found");
+      catch(FileNotFoundException e){
+         System.out.println("File not found.");
       }
    }
+
 }
 
